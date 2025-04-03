2eb925e507b66f755d4aad19adf9067f53900642
dionigc96 <dionigc96@gmail.com>
2025-04-03 09:44:58 +0200

    feat: Some changes in Services section

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,6 +1,28 @@
+import { Box, Paper, Typography } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+
 const Services = () => {
   return (
-    <section id="services" className="services"></section>
+    <section id="services" className="services">
+ <Box textAlign="center" mb={4} mt={5}>
+          <Paper
+            elevation={1}
+            sx={{
+              display: 'inline-flex',
+              alignItems: 'center',
+              padding: '0.5rem 1rem',
+              borderRadius: '20px',
+              backgroundColor: 'var(--white-color)',
+            }}
+          >
+            <PersonIcon sx={{ fontSize: 20, color: 'black', mr: 1 }} />
+            <Typography variant="body2" sx={{ color: 'black' }}>
+              My Services
+            </Typography>
+          </Paper>
+        </Box>
+
+    </section>
   );
 };
 
